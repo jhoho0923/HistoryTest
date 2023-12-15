48a08744fb33d3252bc37c8312a7390544d5e0a2
jhoho82 <jhoho82@gmail.com>
2023-12-15 14:25:41 +0900

    add함수추가

--- Cal.java
+++ Cal.java
@@ -2,6 +2,13 @@
 public class Calculator{
  public static void main(String[] args) {
             // New Work
+	    // add함수추가
+	 int my_add(int number1, int number2){
+
+
+		 
+		return number1+number2;
+	 }
 
      }
 }
