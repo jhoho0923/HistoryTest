807118cc96d4e8266cde95124b4b63856ab82d50
jhoho82 <jhoho82@gmail.com>
2023-12-15 14:19:08 +0900

    주석추가버전으로복원

--- Cal.java
+++ Cal.java
@@ -1,14 +1,10 @@
- 
-import java.util.Scanner;
-public class Calculator {
-    public static void main(String[] args) {
-	    // New Work
-	     int my_add(int number1, int number2){
-		if (number1 < 0 or number2 < 0) // 들어오는 값이 크면(조건절구문)
-			return;
-		return number1+number2;
-		// 정수 값을 반환한다.
 
-	}
-    }
+public class Calculator{
+ public static void main(String[] args) {
+            // New Work
+
+     }
 }
+
+
+
